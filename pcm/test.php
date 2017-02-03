89037dae0e2e6dd141afd9487cbd405b25beafab
Sergey <pmswga@gmail.com>
2017-02-04 00:40:03 +0300

    Реализация PImagesManager

--- pcm/test.php
+++ pcm/test.php
@@ -2,7 +2,9 @@
   
   require_once "structures/pclass.class.php";
   require_once "classes/pimage.class.php";
+  require_once "classes/pimagesmanager.class.php";
   
+  use PCM\Classes\PImagesManager;
   use PCM\Classes\PImage;
   use PCM\Structures\PClass;
   use PCM\Structures\PVar;
@@ -14,10 +16,12 @@
   $class->addVars([new PVar(_PRIVATE_, "c")]);
   
   $image = PImage::import("Smalltalk");
+  $image2 = PImage::import("Smalltalk2");
   
+  $IM = new PImagesManager([$image, $image2]);
   
   echo "<pre>";
-  print_r($image);
+  print_r($IM->getImage("Smalltalk3"));
   echo "</pre>";
   
 ?>
