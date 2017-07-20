e5db778931ab6d23aedb17febca4029e79ac2d7f
Sergey <pmswga@gmail.com>
2017-07-20 21:30:15 +0300

    Add logo

--- src/php/do_it.php
+++ src/php/do_it.php
@@ -6,13 +6,14 @@
 	
 	if (!empty($_POST['code'])) {
 		
-		$classes = array_reverse($_SESSION['tmp_image']->getClasses());
-		
-		foreach ($classes  as $class) {
-			
-			PWorkspace::setClass($class->for_test());
-		}
-
+    if (isset($_SESSION['tmp_image'])) {
+      $classes = array_reverse($_SESSION['tmp_image']->getClasses());
+      foreach ($classes  as $class) {
+        
+        PWorkspace::setClass($class->for_test());
+      }
+    }
+    
 		echo PWorkspace::run($_POST['code']);
 		
 	}
